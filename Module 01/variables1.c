7905bdbf00d4266027e707902e63ae5fceb065a6
hpaes <herbert_pbarreto@hotmail.com>
2020-04-22 23:25:59 -0300

    Create variables1.c

---
+++ Module 01/variables1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    unsigned int idade = 29;
+    double peso = 74.3;
+    char sexo = 'M';
+    char nome[] = "Carlos";
+
+    printf("\n%s é do sexo %c, tem %i anos e pesa %.2f. \n", nome, sexo, idade);
+
+}
