117f011058f37079d9b2d72642686716daa3e6b0
hpaes <herbert_pbarreto@hotmail.com>
2020-04-22 22:36:21 -0300

    Create EX002.c

---
+++ Module 01/EX002.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    printf("\\a\t=\t\tbeep\n");
+    printf("\\n\t=\t\tNova linha\n");
+    printf("\\t\t=\t\tTabulação\n");
+    printf("\\\\ \t=\t\tBarra\n");
+    printf("%%%%\t=\t\tPorcentagem\n");
+    printf("\\?\t=\t\tIntegorração\n");
+    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    
+}
