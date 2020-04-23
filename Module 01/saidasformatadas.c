bd61f23fa142408e86f0a2b40ef8f25f348dbad3
hpaes <herbert_pbarreto@hotmail.com>
2020-04-22 23:07:32 -0300

    Create saidasformatadas.c

---
+++ Module 01/saidasformatadas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("O %s tem %i anos de idade\n", "Herbert", 27);
+    printf("Seu peso atual é de %2.1f Kg\n", 79.1);
+    printf("o seu sexo é %c\n", 'M');
+
+}
