14b8cc584d4bebb702c65b8b60bd343fb4f6e4a6
hpaes <herbert_pbarreto@hotmail.com>
2020-04-22 22:27:23 -0300

    Create EX001.c

---
+++ Module 01/EX001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("\nListagem de Alunos\n");
+    printf("Nome\t\tNota\n");
+    printf("---------------------");
+    printf("\nAna Beatriz\t  8.5\n");
+    printf("Bianca Martinz\t  9.0\n");
+    printf("Cláudio Sá\t  5.5\n");
+    printf("Giovana Silva\t  7.5\n");
+
+}
