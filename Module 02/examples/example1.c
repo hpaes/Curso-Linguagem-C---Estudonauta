67e19187e070fada6de18fcd14c387bf1ad6535f
hpaes <herbert_pbarreto@hotmail.com>
2020-04-25 00:47:42 -0300

    module 2 examples

---
+++ Module 02/examples/example1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float a = 3;
+    int b = 2;
+    float r =  a / b;
+    printf("\nO resultado é: %.2f\n", r);
+    
+    return 0;
+}
---
+++ Module 02/examples/example2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float n1 = 8;
+    float n2 = 7;
+    float fResult = (n1 + n2) / 2;
+
+    printf("\nO resultado é %.2f\n", fResult);
+
+    return 0;
+
+}
