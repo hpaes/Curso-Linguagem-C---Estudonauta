ca0e1d60b5efc86ee45991a06c33cfe024abd9bc
hpaes <herbert_pbarreto@hotmail.com>
2020-04-25 20:13:26 -0300

    Update EX011.c

--- Module 02/desafios/EX011.c
+++ Module 02/desafios/EX011.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(void)
-[
+{   
+    // declarando variáveis
+    int iNumber = 0;
 
-]
+    printf("\n<<<   EX011 - Par ou Ímpar   >>>\n\n");
+    printf("Digite um número qualquer: ");
+    scanf("%d", &iNumber);
+    printf("\nO número %d é %s.\n", iNumber, ((iNumber % 2) == 0)?"Par":"Impar");
+
+    return 0;
+
+}   // end main
