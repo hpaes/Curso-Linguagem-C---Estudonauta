bbf1d1f5040376d06be3e049c0859601c4341b09
hpaes <herbert_pbarreto@hotmail.com>
2020-04-25 18:41:41 -0300

    Create example4.c

---
+++ Module 02/examples/example4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 5;
+    int y = 2;
+
+    // typecasting float
+    float z = (float)x / y;
+
+    printf("\nO resultado é %.1f\n", z);
+    
+    return 0;
+
+}   // end main
