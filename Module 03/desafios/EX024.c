f1616aec6c4b69d3bbd352373f613199516c2a1b
Herbert Paes <8264908+hpaes@users.noreply.github.com>
2020-05-01 16:48:17 -0300

    Create EX024.c

---
+++ Module 03/desafios/EX024.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2;
+
+    printf("\n<<<  EX024 - Ordem em dois números  >>>\n\n");
+    printf("Me diga dois números e eu os colocarei\nos dois em ordem crescente.");
+    printf("\nPrimeiro número: ");
+    scanf(" %d", &n1);
+    printf("\nSegundo número: ");
+    scanf(" %d", &n2);
+
+    if (n1 > n2)
+    {
+        printf("\nOs números em ordem crescente são %d e %d.\n", n2, n1);
+    }
+    else if (n2 > n1)
+    {
+        printf("\nOs números em ordem crescente são %d e %d.\n", n1, n2);
+    }
+    else
+    {
+        printf("\nOs números digitados são iguais.\n");
+    }
+    
+    return 0;
+}   // end main
